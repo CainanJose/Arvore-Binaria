18688f188f3540f10f0432e88bb694a403f11c16
Isabelle Munhoz <isabellescarso@outlook.com>
2024-06-08 14:50:25 -0300

    implementacao de verificacoes e try catch para tratar possiveis entradas invalidas ao programa, e otimizacao das mensagens fornecidas ao usuario

--- Aniversariante.py
+++ Aniversariante.py
@@ -21,10 +21,13 @@ class Aniversariante:
         diaAtual = datetime.now()
 
         if (diaAtual.month, diaAtual.day) == (nasc.month, nasc.day): 
-             meng = "Feliz aniverario!!"
+             mensg = "Feliz aniversario!!"
+        elif(diaAtual.month, diaAtual.day) < (nasc.month, nasc.day):
+            mensg = "Vai fazer aniversario em breve!"
         else:
-            meng = "Vai fazer aniversario!"
-        return meng
+            mensg = "Já fez aniversário este ano!"
+            
+        return mensg
 
     def __str__(self):
         return f"{self.nome} - {self.dia}/{self.mes}/{self.ano} - Idade: {self.idade()} anos \n {self.mensagem()} \n"
